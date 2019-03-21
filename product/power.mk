55fcc3046b905f5546bb4831835e1b49d32365f6
najahiiii <tlogitechnjhi@gmail.com>
2019-03-22 03:16:44 +0700

    rolex/rolex: import caf power hal
    * back to 1.0 power hal
    
    Signed-off-by: najahiiii <tlogitechnjhi@gmail.com>
    Signed-off-by: keizzn <keiusuke.faisal123@gmail.com>

--- product/power.mk
+++ product/power.mk
@@ -1,6 +1,6 @@
-# Power IMPL
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
-
+# Power
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power_profile_riva.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_profile_riva.xml
+    $(LOCAL_PATH)/configs/power_profile_riva.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_profile_riva.xml \
+    power.qcom \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
