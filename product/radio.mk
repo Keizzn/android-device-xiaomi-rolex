8dcef9c118c3b0f6ef353ae802b3072c518a0cad
keizzn <keiusuke.faisal123@gmail.com>
2019-05-22 16:45:24 +0700

    product: radio: build extra copies of telephony-ext
    Signed-off-by: keizzn <keiusuke.faisal123@gmail.com>

--- product/radio.mk
+++ product/radio.mk
@@ -73,6 +73,9 @@ PRODUCT_PACKAGES += \
     qti-telephony-common \
     ims-ext-common
 
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
